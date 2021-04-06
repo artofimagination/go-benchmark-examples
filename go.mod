b815042e3f98f11c46ae8fc014a9c177434528d1
Art of Science and Imagination <artofimag@gmail.com>
2021-04-07 10:27:14 +1200

    Add testing passing values/ptr-s to function

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/artofimagination/go-benchmark-examples
+
+go 1.15
+
+require (
+	github.com/artofimagination/golang-docker v0.0.3
+	github.com/artofimagination/mysql-user-db-go-interface v0.0.19
+	github.com/google/uuid v1.1.5
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/kr/pretty v0.2.1
+	github.com/pion/webrtc/v3 v3.0.14
+	github.com/pkg/errors v0.9.1
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	golang.org/x/perf v0.0.0-20210220033136-40a54f11e909 // indirect
+)
---
+++ tests/pass-value-and-pointer-to-function_test.go
@@ -0,0 +1,216 @@
+package tests
+
+// go get golang.org/x/perf/cmd/benchstat
+// Command go test ./... -bench=BenchmarkPassValue -benchmem -run=^$ -count=10 > head.txt && benchstat head.txt
+// Command go test ./... -bench=BenchmarkPassPointer -benchmem -run=^$ -count=10 > head.txt && benchstat head.txt
+
+import (
+	"os"
+	"runtime/trace"
+	"testing"
+)
+
+type TestData struct {
+	a, b, c string
+	d, e, f int
+	g, h, i float64
+}
+
+func copyByValue(test TestData) int {
+	test2 := test
+	return test2.d
+}
+
+func copyByPointer(test *TestData) int {
+	test2 := *test
+	return test2.d
+}
+
+type Class struct {
+}
+
+func (c Class) copyByValueValueReceiver(test TestData) int {
+	test2 := test
+	return test2.d
+}
+
+func (c Class) copyByPointerValueReceiver(test *TestData) int {
+	test2 := *test
+	return test2.d
+}
+
+func (c *Class) copyByValuePointerReceiver(test TestData) int {
+	test2 := test
+	return test2.d
+}
+
+func (c *Class) copyByPointerPointerReceiver(test *TestData) int {
+	test2 := *test
+	return test2.d
+}
+
+func BenchmarkPassValue(b *testing.B) {
+	test := TestData{
+		a: "test1", b: "test2", c: "test3",
+		d: 3, e: 4, f: 5,
+		g: 2.3, h: 4.5, i: 6.7,
+	}
+
+	f, err := os.Create("byValue.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = copyByValue(test)
+	}
+
+	trace.Stop()
+
+	b.StopTimer()
+}
+
+func BenchmarkPassPointer(b *testing.B) {
+	test := TestData{
+		a: "test1", b: "test2", c: "test3",
+		d: 3, e: 4, f: 5,
+		g: 2.3, h: 4.5, i: 6.7,
+	}
+
+	f, err := os.Create("byPointer.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = copyByPointer(&test)
+	}
+
+	trace.Stop()
+
+	b.StopTimer()
+}
+
+func BenchmarkPassValueValueReceiver(b *testing.B) {
+	class := Class{}
+	test := TestData{
+		a: "test1", b: "test2", c: "test3",
+		d: 3, e: 4, f: 5,
+		g: 2.3, h: 4.5, i: 6.7,
+	}
+
+	f, err := os.Create("byValueVRec.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = class.copyByValueValueReceiver(test)
+	}
+
+	trace.Stop()
+
+	b.StopTimer()
+}
+
+func BenchmarkPassPointerValueReceiver(b *testing.B) {
+	class := Class{}
+	test := TestData{
+		a: "test1", b: "test2", c: "test3",
+		d: 3, e: 4, f: 5,
+		g: 2.3, h: 4.5, i: 6.7,
+	}
+
+	f, err := os.Create("byPointerVRec.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = class.copyByPointerValueReceiver(&test)
+	}
+
+	trace.Stop()
+
+	b.StopTimer()
+}
+
+func BenchmarkPassValuePointerReceiver(b *testing.B) {
+	class := Class{}
+	test := TestData{
+		a: "test1", b: "test2", c: "test3",
+		d: 3, e: 4, f: 5,
+		g: 2.3, h: 4.5, i: 6.7,
+	}
+
+	f, err := os.Create("byValuePtrRec.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = class.copyByValuePointerReceiver(test)
+	}
+
+	trace.Stop()
+
+	b.StopTimer()
+}
+
+func BenchmarkPassPointerPointerReceiver(b *testing.B) {
+	class := Class{}
+	test := TestData{
+		a: "test1", b: "test2", c: "test3",
+		d: 3, e: 4, f: 5,
+		g: 2.3, h: 4.5, i: 6.7,
+	}
+
+	f, err := os.Create("byPtrPtrRec.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = class.copyByPointerPointerReceiver(&test)
+	}
+
+	trace.Stop()
+
+	b.StopTimer()
+}
